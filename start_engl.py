30315bf45c31f19cbc565e0313779064f2b7f6a5
supaeasy <59504964+supaeasy@users.noreply.github.com>
2024-10-04 01:35:27 +0200

    Update start_engl.py
    Added Domain extractor to shorten links and ensure non link-sources like "Grandma" are shown correctly

--- start_engl.py
+++ start_engl.py
@@ -1,31 +1,50 @@
-import requests, re, os
+import requests
+import re
+import os
 from jinja2 import Environment, FileSystemLoader, Template
 from fractions import Fraction
+from urllib.parse import urlparse
 
-# Convert Decimals into fractions
+def extract_domain(url):
+    if not url:
+        return None
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    # Remove "www." Prefix
+    domain = re.sub(r'^www\.', '', domain)
+    return domain if domain else None
+
+def fetch_recipe_data(recipe_id):
+    """Fetches recipe Data from API and extracts domain from source_url"""
+    recipe_url = f"[YOUR_TANDOOR_HOST]/api/recipe/{recipe_id}/"
+    response = requests.get(recipe_url, headers=headers)
+    if response.status_code == 200:
+        recipe_data = response.json()
+        if 'source_url' in recipe_data and recipe_data['source_url']:
+            extracted_domain = extract_domain(recipe_data['source_url'])
+            recipe_data['source_domain'] = extracted_domain if extracted_domain else recipe_data['source_url']
+        else:
+            recipe_data['source_domain'] = None
+        return recipe_data
+    else:
+        print(f"Recipe-ID {recipe_id} could not be found (Status: {response.status_code}).")
+        return None
+
 def decimal_to_nicefrac(value):
-    # Convert value to float
     try:
         value = float(value)
     except ValueError:
-        return value  # if not number, return value
-
-    # Return if integer
+        return value
     if value.is_integer():
         return str(int(value))
-    
-    # Convert Number into fraction
-    frac = Fraction(value).limit_denominator(8)  # Limit denominator to 8, for clean fractions such as 1/2, 1/4 
-    
-    # Extract numerator and denominator
+    frac = Fraction(value).limit_denominator(8)
     if frac.numerator > frac.denominator:
         whole_number = frac.numerator // frac.denominator
         fraction_part = frac - whole_number
         return f"{whole_number}\\nicefrac{{{fraction_part.numerator}}}{{{fraction_part.denominator}}}"
     else:
         return f"\\nicefrac{{{frac.numerator}}}{{{frac.denominator}}}"
-        
-# Define Replacement functions for '°C' und 'Min.'
+
 def replace_celsius(value):
     return value.replace("°C", r"\textcelcius")
 
@@ -36,7 +55,6 @@ def replace_min_space(value):
         value = re.sub(r'(\d+)\s+Minuten', r'\1~Minuten', value)
     return value
 
-# Adjustment of jinja2 environment to avoid curly brackets that are heavily used in LaTeX.
 env = Environment(
     loader=FileSystemLoader("templates"),
     block_start_string="<<%",
@@ -47,115 +65,70 @@ env = Environment(
     comment_end_string="#>>"
 )
 
-# Register user defined filters in Jinja2-environment
 env.filters['replace_celsius'] = replace_celsius
 env.filters['replace_min_space'] = replace_min_space
 env.filters['decimal_to_nicefrac'] = decimal_to_nicefrac
 
-# API-URL to fetch total number of recipes
-api_url = "[YOUR_TANDOOR_HOST]/api/recipe/" #replace with your info
+api_url = "[YOUR_TANDOOR_HOST]/api/recipe/"
 headers = {
-    "Authorization": "Bearer tda_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx" #your API token goes here
+    "Authorization": "Bearer [YOUR_TANDOOR_TOKEN]"
 }
 
-# API-call, to fetch total number of recipes
 response = requests.get(api_url, headers=headers)
-
 if response.status_code == 200:
     data = response.json()
-    total_count = data['count']  # Get recipe count
+    total_count = data['count']
     print(f"{total_count} Recipes found.")
 else:
     print(f"Error: {response.status_code} - {response.text}")
     total_count = 0
 
-# User choice
 choice = input("Would you like to export a certain recipe (enter ID) or all (a)? ")
 
-# Load Template
 template = env.get_template('xcookybooky-nswissgerman-11pt.txt')
 
-
-
 def download_recipe_image(recipe_data, recipe_name):
-    """Downloads image and saves to Pictures"""
     if 'image' in recipe_data and recipe_data['image']:
         image_url = recipe_data['image']
         image_response = requests.get(image_url)
-        
         if image_response.status_code == 200:
-            image_extension = os.path.splitext(image_url)[1]  
+            image_extension = os.path.splitext(image_url)[1]
             image_path = os.path.join(pictures_dir, f"{recipe_name}{image_extension}")
-            
-            # Speichere das Bild im Pictures-Ordner
             with open(image_path, 'wb') as image_file:
                 image_file.write(image_response.content)
-            
             print(f"Picture for {recipe_name} downloaded successfully.")
         else:
             print(f"Picture could not be downloaded: {image_url}")
     else:
         print(f"No picture found for {recipe_name}.")
 
-# Folder for exported LaTeX-Files
 output_dir = "exported_recipes"
 os.makedirs(output_dir, exist_ok=True)
 
-# Create Folder for picture downloads
 pictures_dir = os.path.join(output_dir, "Pictures")
 os.makedirs(pictures_dir, exist_ok=True)
 
-def fetch_recipe_data(recipe_id):
-    """ Gets selected recipe from API """
-    recipe_url = f"[YOUR_TANDOOR_HOST]/api/recipe/{recipe_id}/" #replace with your info
-    response = requests.get(recipe_url, headers=headers)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Recipe ID {recipe_id} could not be found (Status: {response.status_code}).")
-        return None
-
-if choice.lower() == 'a':  # Export all Recipes
-    for recipe_id in range(1, total_count + 1):  # Crawl through all IDs
-        recipe_data = fetch_recipe_data(recipe_id)  # Fetch recipe Data from recipe
-        
+if choice.lower() == 'a':
+    for recipe_id in range(1, total_count + 1):
+        recipe_data = fetch_recipe_data(recipe_id)
         if recipe_data:
-            # Name file like recipe (Replace unwanted characters)
-            recipe_name = recipe_data['name'].replace("/", "-")  
+            recipe_name = recipe_data['name'].replace("/", "-")
             output_path = os.path.join(output_dir, f"{recipe_name}.tex")
-            
-            # Render Template
             latex_content = template.render(recipe=recipe_data)
-            
-            # Save rendered LaTeX-File
             with open(output_path, 'w') as file:
                 file.write(latex_content)
-            # Download recipe image
             download_recipe_image(recipe_data, recipe_name)
-            
             print(f"{recipe_name}.tex exported successfully.")
 else:
-    # User enters recipe ID
     recipe_id = int(choice)
-    
-    # Fetch recipe Data
     recipe_data = fetch_recipe_data(recipe_id)
-    
     if recipe_data:
-        # Name file like recipe (Replace unwanted characters)
         recipe_name = recipe_data['name'].replace("/", "-")
         output_path = os.path.join(output_dir, f"{recipe_name}.tex")
-        
-        # Render Template
         latex_content = template.render(recipe=recipe_data)
-        
-        # Save rendered LaTeX-File
         with open(output_path, 'w') as file:
             file.write(latex_content)
-        # Download recipe image
         download_recipe_image(recipe_data, recipe_name)
-        
         print(f"{recipe_name}.tex exported successfully.")
     else:
         print(f"Recipe ID {recipe_id} not found.")
